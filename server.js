49f4c1300d34311d72951dd313da7788ccabaaf3
Sarah <sweetiejeremy@hotmail.com>
2017-05-18 15:48:28 -0700

    Added create route

--- server.js
+++ server.js
@@ -32,6 +32,40 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/views/firstpage.html'));
 });
 
+apiRoutes.get('/',function(req, res){
+     Blog.find({}, function(err, allblogs){
+        if(err){
+            console.log(err);
+        } else {
+            res.send(JSON.stringify({blogs: allblogs}));
+        }
+    });
+    
+});
+
+app.post("/blog/new", passport.authenticate('jwt', { session: false }), function(req, res){
+    // get data from form and add to campgrounds array
+var newBlog = {
+     name : req.body.name,
+     image : req.body.image,
+     desc : req.body.description,
+     author : {
+        'id': req.user._id,
+        'username': req.user.username
+    },
+};    
+// Create a new blog and save to DB
+    Blog.create(newBlog, function(err, newlyCreated){
+        if(err) {
+          return res.json({ success: false, message: 'Cannot create blog.', });
+        } else {
+            // redirect to blog page
+            console.log(newlyCreated);
+            res.json({ success: true, message: 'Successfully created new blog.' });
+        }
+    });
+});
+
 app.listen(3000, function(req, res){
    console.log("Server has started");
 });
